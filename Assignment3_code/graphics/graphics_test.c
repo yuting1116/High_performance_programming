2ef8ecfaf4b9aa808647a61f7226ef70a5bfa603
yuting1116 <81543468+yuting1116@users.noreply.github.com>
2026-02-10 13:10:51 +0100

    Create folder

---
+++ Assignment3_code/graphics/graphics_test.c
@@ -0,0 +1,58 @@
+/*
+ * Example of how to use graphics routines.
+ *
+ * Based on code by Oliver Fringer, Stanford University.
+ * 
+ * Modified by Elias Rudberg, Uppsala University.
+ *
+ */
+
+#include "graphics.h"
+#include <math.h>
+
+const float circleRadius=0.025, circleColor=0;
+const int windowWidth=800;
+
+void keep_within_box(float* xA, float* yA) {
+  if(*xA > 1)
+    *xA = 0;
+  if(*yA > 1)
+    *yA = 0;
+}
+
+int main(int argc, char *argv[]) {
+  float L=1, W=1;
+
+  float xA = 0.45;
+  float yA = 0.90;
+  float xB = 0.32;
+  float yB = 0.56;
+
+  InitializeGraphics(argv[0],windowWidth,windowWidth/2);
+  SetCAxes(0,1);
+
+  printf("Hit q to quit.\n");
+  while(!CheckForQuit()) {
+    /* Move A. */
+    xA += 0.0012;
+    yA += 0.0020;
+    printf("%.5f, %.5f", xA, yA);
+    keep_within_box(&xA, &yA);
+    printf("-> %.5f, %.5f\n", xA, yA);
+    /* Move B. */
+    // xB += 0.0007;
+    // yB += 0.0018;
+    keep_within_box(&xB, &yB);
+    /* Call graphics routines. */
+    ClearScreen();
+    DrawCircle(xA, yA, L, W, circleRadius, circleColor);
+    DrawCircle(xB, yB, L, W, circleRadius, circleColor);
+    Refresh();
+    /* Sleep a short while to avoid screen flickering. */
+    usleep(100000);
+  }
+  FlushDisplay();
+  CloseDisplay();
+  return 0;
+}
+
---
+++ Assignment3_code/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# reference folder
+input_folder="./input_data"
+ref_folder="./ref_output_data"
+out_folder="./graphics"
+
+# N 值列表
+Ns=(10 100 500)
+#  1000 2000 3000)
+# , 100, 500, 1000, 2000, 3000)
+
+for N in "${Ns[@]}"; do
+
+    ref_file="${ref_folder}/ellipse_N_$(printf "%05d" $N)_after200steps.gal"
+    out_file="./result.gal"
+    input_file="${input_folder}/ellipse_N_$(printf "%05d" $N).gal"
+
+    "${out_folder}/galsim" "$N" "$input_file" 200 0.00001 0
+    "./compare_gal_files/a.out" "$N" "$ref_file" "$out_file"
+done
